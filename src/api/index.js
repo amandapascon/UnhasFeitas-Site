ce4a9a19e2cc78ed622236e2d9158b33bcff838e
Amanda <amanda.pascon@estudante.ufscar.br>
2021-10-29 09:13:14 -0300

    .

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 export const server = axios.create(
-    {baseURL: "https://unhas-feitas-server.herokuapp.com/"
+    {baseURL: "https://unhas-feitas-api.herokuapp.com/"
 })
 
 /* export const server = axios.create(
     {baseURL: "http://localhost:8080"
-}) */
+})
+ */
